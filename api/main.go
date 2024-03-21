c6c9f2fbe50cf72d605ea2918e3f8864eb23f7f4
Richard Jones <richard.jones@aquia.io>
2024-03-21 15:56:03 -0600

    refactored config handling

--- api/main.go
+++ api/main.go
@@ -4,36 +4,21 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/caarlos0/env/v10"
-
-	"github.com/CMS-Enterprise/ztmf/api/graphql"
+	"github.com/CMS-Enterprise/ztmf/api/engine"
 )
-
-type Config struct {
-	ENV       string `env:"ENVIRONMENT" envDefault:"local"`
-	PORT      string `env:"PORT" envDefault:"3000"`
-	CERT_FILE string `env:"CERT_FILE"`
-	KEY_FILE  string `env:"KEY_FILE"`
-}
 
 func main() {
 
-	var err error
+	cfg := engine.GetConfig()
 
-	cfg := Config{}
-	err = env.Parse(&cfg)
-	if err != nil {
-		log.Fatalf("unable to parse ennvironment variables: %e", err)
-	}
-
-	http.Handle("/graphql", graphql.GetHttpHandler())
+	http.Handle("/graphql", engine.GetHttpHandler())
 
 	log.Printf("%s environment listening on %s\n", cfg.ENV, cfg.PORT)
-
 	switch cfg.ENV {
 	case "local":
 		log.Fatal(http.ListenAndServe(":"+cfg.PORT, nil))
 	case "dev", "prod":
 		log.Fatal(http.ListenAndServeTLS(":"+cfg.PORT, cfg.CERT_FILE, cfg.KEY_FILE, nil))
 	}
+
 }
