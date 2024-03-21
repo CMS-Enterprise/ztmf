c5a6176da2e1435a967eb5218bdcad83af222c5e
Richard Jones <richard.jones@aquia.io>
2024-03-21 17:05:47 -0600

    moving schema to string instead of file to avoid pathing issues in container

--- api/engine/graphql.go
+++ api/engine/graphql.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"log"
-	"os"
 
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
@@ -11,12 +10,7 @@ import (
 type rootResolver struct{}
 
 func GetHttpHandler() *relay.Handler {
-	rawSchemaBytes, err := os.ReadFile("schema.graphql") // just pass the file name
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	schema, err := graphql.ParseSchema(string(rawSchemaBytes), &rootResolver{})
+	schema, err := graphql.ParseSchema(schema, &rootResolver{})
 	if err != nil {
 		log.Fatal(err)
 	}
