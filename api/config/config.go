c14fa58e7ef3f6a19fe24747416317f89cbccb10
Richard A. Jones <richard.jones@aquia.io>
2024-04-03 14:56:56 -0600

    refactored db and config into packages (#12)
    * refactored db and config into packages
    
    * removed default values

---
+++ api/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+
+	"github.com/caarlos0/env/v10"
+)
+
+type dbconfig struct {
+	User     string `json:"username"`
+	Pass     string `json:"password"`
+	RawCreds string `env:"DB_CREDS"`
+	Database string `env:"DB_NAME"`
+	Endpoint string `env:"DB_ENDPOINT"`
+	Port     string `env:"DB_PORT" envDefault:"5432"`
+}
+
+type config struct {
+	ENV       string `env:"ENVIRONMENT" envDefault:"local"`
+	PORT      string `env:"PORT" envDefault:"3000"`
+	CERT_FILE string `env:"CERT_FILE"`
+	KEY_FILE  string `env:"KEY_FILE"`
+	DB        *dbconfig
+}
+
+var cfg *config
+
+func GetConfig() *config {
+
+	if cfg == nil {
+		log.Print("Initializing config...")
+
+		var once sync.Once
+		once.Do(func() {
+			cfg = &config{DB: &dbconfig{}}
+			err := env.Parse(cfg)
+			if err != nil {
+				log.Fatalf("unable to parse environment variables: %e", err)
+			}
+
+			err = json.Unmarshal([]byte(cfg.DB.RawCreds), cfg.DB)
+			if err != nil {
+				log.Fatalf("unable to parse db creds: %e", err)
+			}
+		})
+	}
+
+	return cfg
+}
