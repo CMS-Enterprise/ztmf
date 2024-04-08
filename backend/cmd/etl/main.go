d68adc10cbae1363d28946bec9d367d9e50d92c6
Richard A. Jones <richard.jones@aquia.io>
2024-04-08 14:00:25 -0600

    feature: ETL for csv to functionscores table

---
+++ backend/cmd/etl/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/CMS-Enterprise/ztmf/backend/internal/db"
+)
+
+func main() {
+	db.GetPool()
+
+	inputCsvFile := os.Args[1]
+	log.Println("Opening CSV", inputCsvFile, "...")
+	file, err := os.Open(inputCsvFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Reading records from CSV...")
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	headers := rows[0]           // headers, if present, are always row 0
+	functionNames := headers[5:] // function NAMES are in the headers, VALUES are in the rows starting with column F
+	records := rows[1:]
+	count := 0
+
+	log.Println("Processing scores...")
+	// iterate over the records
+	for _, columns := range records {
+		acronym := columns[0]
+		datacenterenvironment := columns[3]
+		fismaSystemId := getFismaSystemId(acronym)
+
+		// collection of scores+notes pairs start at column F
+		scoresNotes := columns[5:]
+
+		// iterate through the score+notes columns by 2s
+		for ii := 0; ii < len(scoresNotes); ii += 2 {
+			if scoresNotes[ii] == "" {
+				// in case we have trailing empty columns
+				continue
+			}
+
+			functionName := functionNames[ii]
+			functionId := getFunctionId(functionName, datacenterenvironment)
+			score, _ := strconv.ParseFloat(scoresNotes[ii][0:1], 32) // first char
+			note := scoresNotes[ii+1]                                // notes are always to the right of the score
+			funcScore := &functionScore{fismaSystemId, functionId, score, note}
+			funcScore.save()
+		}
+		count++
+	}
+
+	log.Printf("FINISHED processing %d records.\n", count)
+}
+
+func getFismaSystemId(acronym string) int {
+	dbpool := db.GetPool()
+	row := dbpool.QueryRow(context.Background(), "SELECT fismasystemid FROM fismasystems WHERE UPPER(fismaacronym)=UPPER($1)", acronym)
+
+	var fismaSystemId int
+	err := row.Scan(&fismaSystemId)
+	if err != nil {
+		log.Printf("fismasystemid could not be found with fismaacronym: %s\n", acronym)
+	}
+
+	return fismaSystemId
+}
+
+func getFunctionId(functionName, datacenterenvironment string) int {
+	dbpool := db.GetPool()
+	row := dbpool.QueryRow(context.Background(), "SELECT functionid FROM functions WHERE LOWER(function)=LOWER($1) AND LOWER(datacenterenvironment)=LOWER($2)", functionName, datacenterenvironment)
+
+	var functionid int
+
+	err := row.Scan(&functionid)
+	if err != nil {
+		log.Printf("functionid could not be found with function: %s and datacenterenvironment:%s\n", functionName, datacenterenvironment)
+	}
+
+	return functionid
+}
+
+type functionScore struct {
+	fismasystemid int
+	functionid    int
+	score         float64
+	notes         string
+}
+
+func (fs *functionScore) save() {
+	dbpool := db.GetPool()
+
+	_, err := dbpool.Exec(context.Background(), "INSERT INTO functionscores(fismasystemid,functionid,datecalculated,score,notes) VALUES($1,$2,TO_TIMESTAMP('2024-09-01 12:00:00','YYYY-MM-DD HH:MI:SS'),$3,$4)", fs.fismasystemid, fs.functionid, fs.score, fs.notes)
+	if err != nil {
+		log.Println("function score could not be saved", err)
+	}
+}
