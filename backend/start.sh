2debaf68479bd867a0040458de9fb7390f93a684
Richard Jones <richard.jones@aquia.io>
2024-04-09 11:25:31 -0600

    moved start.sh functionality to zsh function to keep it out of repo

--- backend/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DB_CREDS=$(aws --profile ztmf-dev secretsmanager get-secret-value --secret-id $DB_CREDS_SECRET_ID --query 'SecretString' --output text)
-export DB_NAME="ztmf"
-go run ./...
