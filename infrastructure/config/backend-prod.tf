88b3ef988031938dd723bf5f854927fdcd2da5cd
Richard Jones <richard.jones@aquia.io>
2024-04-25 12:15:44 -0600

    added prod specific details

---
+++ infrastructure/config/backend-prod.tf
@@ -0,0 +1,4 @@
+bucket  = "ztmf-terraform-state-use1-prod"
+key     = "tfstate"
+region  = "us-east-1"
+profile = "ztmf-prod"
---
+++ infrastructure/tfvars/prod.tfvars
@@ -0,0 +1,3 @@
+# specific to ztmf dev account
+environment        = "prod"
+domain_name_prefix = ""
