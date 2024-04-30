76341e37f5754bf9fddf677488814beb41e29415
Richard Jones <richard.jones@aquia.io>
2024-04-30 11:54:02 -0600

    refactored to pull all oidc fields from secret

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -6,4 +6,9 @@ locals {
   db_cred_secret = join("",data.aws_secretsmanager_secrets.rds.arns)
 
   domain_name = "${var.domain_name_prefix}ztmf.cms.gov"
+
+  // simplify referencing of json object fields for aws_verifiedaccess_trust_provider.ztmf_idmokta.oidc_options
+  // technically only one of the fields was a true secret (client_secret), but since we have the space here
+  //  we can use it to simplify code instead of placing all the other fields in TF vars
+  oidc_options = jsondecode(data.aws_secretsmanager_secret_version.ztmf_va_trust_provider_current.secret_string)
 }
