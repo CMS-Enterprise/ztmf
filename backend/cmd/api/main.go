b67623542c225b128909587da6ade296fb9fdee0
Richard Jones <richard.jones@aquia.io>
2024-06-07 13:41:39 -0600

    ensure api is using FIPS compliant ciphers

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	_ "crypto/tls/fipsonly"
 	"log"
 	"net/http"
 
@@ -19,8 +21,32 @@ func main() {
 	http.Handle("/graphql", handler)
 
 	log.Printf("%s environment listening on %s\n", cfg.Env, cfg.Port)
+
 	if cfg.CertFile != "" && cfg.KeyFile != "" {
-		log.Fatal("could not listen and serve:", http.ListenAndServeTLS(":"+cfg.Port, cfg.CertFile, cfg.KeyFile, nil))
+		cert, err := tls.LoadX509KeyPair(cfg.CertFile, cfg.KeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+			},
+			MinVersion: tls.VersionTLS13,
+		}
+
+		server := &http.Server{
+			Addr:      cfg.Port,
+			Handler:   handler,
+			TLSConfig: tlsConfig,
+		}
+		err = server.ListenAndServeTLS("", "")
+		if err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+
 	} else {
 		log.Fatal("could not listen and serve:", http.ListenAndServe(":"+cfg.Port, nil))
 	}
