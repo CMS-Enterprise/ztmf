adde3a3fa16520e0059e528eea463b724994d639
Richard A. Jones <richard.jones@aquia.io>
2024-06-10 13:28:48 -0600

    fix: ensure api and load balancer are FIPS compliant  (#53)
    Added `ELBSecurityPolicy-TLS13-1-3-FIPS-2023-04` security policy to ALB
    Configured HTTP listener to use TLS 1.3 and log TLS version and cipher
    for all requests
    
    fixes #50

--- backend/cmd/api/handler.go
+++ backend/cmd/api/handler.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 )
 
 type rootResolver struct{}
 
-func HttpHandler() (*relay.Handler, error) {
+var tlsConstants = map[uint16]string{
+	0x1301: "TLS_AES_128_GCM_SHA256",
+	0x1302: "TLS_AES_256_GCM_SHA384",
+	0x1303: "TLS_CHACHA20_POLY1305_SHA256",
+	0x0301: "VersionTLS10",
+	0x0302: "VersionTLS11",
+	0x0303: "VersionTLS12",
+	0x0304: "VersionTLS13",
+}
+
+func HttpHandler() (http.Handler, error) {
 	schema, err := graphql.ParseSchema(schema, &rootResolver{})
 	if err != nil {
 		return nil, err
 	}
 
-	return &relay.Handler{Schema: schema}, nil
+	return logRequest(&relay.Handler{Schema: schema}), nil
+}
+
+func logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\r", tlsConstants[r.TLS.Version], tlsConstants[r.TLS.CipherSuite])
+		next.ServeHTTP(w, r)
+	})
 }
--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 
@@ -8,7 +9,7 @@ import (
 )
 
 func main() {
-
+	log.SetFlags(0)
 	cfg := config.GetInstance()
 
 	handler, err := HttpHandler()
@@ -19,8 +20,33 @@ func main() {
 	http.Handle("/graphql", handler)
 
 	log.Printf("%s environment listening on %s\n", cfg.Env, cfg.Port)
+
 	if cfg.CertFile != "" && cfg.KeyFile != "" {
-		log.Fatal("could not listen and serve:", http.ListenAndServeTLS(":"+cfg.Port, cfg.CertFile, cfg.KeyFile, nil))
+		log.Print("Loading TLS configuration")
+		cert, err := tls.LoadX509KeyPair(cfg.CertFile, cfg.KeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+			},
+			MinVersion: tls.VersionTLS13,
+		}
+
+		server := &http.Server{
+			Addr:      ":" + cfg.Port,
+			Handler:   handler,
+			TLSConfig: tlsConfig,
+		}
+		err = server.ListenAndServeTLS("", "")
+		if err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+
 	} else {
 		log.Fatal("could not listen and serve:", http.ListenAndServe(":"+cfg.Port, nil))
 	}
