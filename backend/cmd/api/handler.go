8c2a4928405d8eca99adfee9b0cbe7a97aab974d
Richard Jones <richard.jones@aquia.io>
2024-06-26 14:28:27 -0600

    remove TLS config logging

--- backend/cmd/api/handler.go
+++ backend/cmd/api/handler.go
@@ -10,16 +10,6 @@ import (
 
 type rootResolver struct{}
 
-var tlsConstants = map[uint16]string{
-	0x1301: "TLS_AES_128_GCM_SHA256",
-	0x1302: "TLS_AES_256_GCM_SHA384",
-	0x1303: "TLS_CHACHA20_POLY1305_SHA256",
-	0x0301: "VersionTLS10",
-	0x0302: "VersionTLS11",
-	0x0303: "VersionTLS12",
-	0x0304: "VersionTLS13",
-}
-
 func HttpHandler() (http.Handler, error) {
 	schema, err := graphql.ParseSchema(schema, &rootResolver{})
 	if err != nil {
@@ -31,11 +21,7 @@ func HttpHandler() (http.Handler, error) {
 
 func logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// only needed for debug
-		// cfg := config.GetInstance()
-		// if cfg.CertFile != "" && cfg.KeyFile != "" {
-		// 	log.Printf("%s %s\r", tlsConstants[r.TLS.Version], tlsConstants[r.TLS.CipherSuite])
-		// }
+		// TODO: log requests
 		next.ServeHTTP(w, r)
 	})
 }
