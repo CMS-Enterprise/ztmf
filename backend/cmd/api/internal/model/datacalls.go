5fa4c7fafaa219aff6ba2027c0d37a33eebe4645
Richard A. Jones <richard.jones@aquia.io>
2024-08-23 10:09:46 -0600

    feat: added route for datacalls (#100)
    this also moves all routes to /api/v1

---
+++ backend/cmd/api/internal/model/datacalls.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type DataCall struct {
+	DataCallID  int32   `json:"datacallid"`
+	DataCall    string  `json:"datacall"`
+	DateCreated float64 `json:"datecreated"`
+	Deadline    float64 `json:"deadline"`
+}
+
+func FindDataCalls(ctx context.Context) ([]*DataCall, error) {
+	sqlb := sqlBuilder.Select("datacallid, datacall, EXTRACT(EPOCH FROM datecreated) as datecreated, EXTRACT(EPOCH FROM deadline) as deadline").
+		From("datacalls")
+
+	sql, boundArgs, _ := sqlb.ToSql()
+	rows, err := query(ctx, sql, boundArgs...)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return pgx.CollectRows(rows, func(row pgx.CollectableRow) (*DataCall, error) {
+		datacall := DataCall{}
+		err := rows.Scan(&datacall.DataCallID, &datacall.DataCall, &datacall.DateCreated, &datacall.Deadline)
+		return &datacall, err
+	})
+}
--- backend/db-schema.sql
+++ backend/db-schema.sql
@@ -1,4 +1,12 @@
-CREATE TABLE IF NOT EXISTS public.fismasystems
+CREATE TABLE public.datacalls
+(
+    datacallid SERIAL NOT NULL PRIMARY KEY,
+    datacall character(9) NOT NULL,
+    datecreated timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deadline timestamp with time zone NOT NULL
+);
+
+CREATE TABLE public.fismasystems
 (
     fismasystemid SERIAL PRIMARY KEY,
     fismauid varchar(255) NOT NULL,
@@ -12,7 +20,7 @@ CREATE TABLE IF NOT EXISTS public.fismasystems
     datacenterenvironment varchar(255),
     datacallcontact varchar(255),
     issoemail varchar(255)
-) TABLESPACE pg_default;
+);
 
 CREATE TABLE public.functions (
     functionid SERIAL PRIMARY KEY,
@@ -24,16 +32,29 @@ CREATE TABLE public.functions (
     advanced varchar(1024),
     optimal varchar(1024),
     datacenterenvironment varchar(255)
-) TABLESPACE pg_default;
+);
 
-CREATE TABLE public.functionscores (
-  scoreid SERIAL PRIMARY KEY,
-  fismasystemid INT NOT NULL,
-  functionid INT NOT NULL,
-  datecalculated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  score FLOAT NOT NULL,
-  notes varchar(1000)
-) TABLESPACE pg_default;
+CREATE TABLE public.functionoptions (
+    functionoptionid SERIAL NOT NULL,
+    functionid integer NOT NULL,
+    score integer NOT NULL,
+    optionname character varying(30) NOT NULL,
+    description character varying(1024),
+    CONSTRAINT functionoptions_pkey PRIMARY KEY (functionoptionid),
+    CONSTRAINT functionoptions_functionid_fkey FOREIGN KEY (functionid)
+        REFERENCES public.functions (functionid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+CREATE TABLE public.scores (
+    scoreid SERIAL NOT NULL DEFAULT PRIMARY KEY,
+    fismasystemid integer NOT NULL,
+    datecalculated timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    notes character varying(1000),
+    functionoptionid integer NOT NULL,
+    datacallid integer NOT NULL,
+);
 
 CREATE TYPE roles AS ENUM ('ISSO','ISSM','ADMIN');
 
