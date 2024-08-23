45dc514a1de910c997cae0a62646e0c706de3822
Richard Jones <richard.jones@aquia.io>
2024-08-23 12:22:41 -0600

    added login to redirect to IDM/Okta

--- backend/cmd/api/internal/router/router.go
+++ backend/cmd/api/internal/router/router.go
@@ -20,13 +20,13 @@ func Handler() http.Handler {
 
 	router.HandleFunc("/api/v1/functions/{functionid}/options", controller.ListFunctionOptions).Methods("GET")
 
-	router.HandleFunc("/api/v1/users/{email:[a-zA-Z0-9.]+@cms.hhs.gov}", controller.GetUserByEmail).Methods("GET")
+	router.HandleFunc("/api/v1/users/{email:[a-zA-Z0-9.]+@[a-zA-Z0-9.]+}", controller.GetUserByEmail).Methods("GET")
 	router.HandleFunc("/api/v1/users/{userid:[a-zA-Z0-9\\-]+}", controller.GetUserById).Methods("GET")
 
 	router.HandleFunc("/api/v1/scores", controller.ListScores).Queries("datacallid", "{datacallid:[0-9]+}", "fismasystemid", "{fismasystemid:[0-9]+}").Methods("GET")
 	router.HandleFunc("/api/v1/scores", controller.SaveScore).Methods("POST")
 	router.HandleFunc("/api/v1/scores/{scoreid}", controller.SaveScore).Methods("PUT")
 
-	router.HandleFunc("/api/v1/whoami", controller.WhoAmI).Methods("GET")
+	router.HandleFunc("/whoami", controller.WhoAmI).Methods("GET")
 	return router
 }
