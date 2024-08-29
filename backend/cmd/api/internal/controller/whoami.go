d3e4bc91cc48700fff21c0c6ecb45425381d380e
Richard A. Jones <richard.jones@aquia.io>
2024-08-29 09:35:55 -0600

    refactor: replace /whoami with /users/current (#106)
    `/whoami` was returning a JWT from which the email was then used to call
    `/users/<email>`
    The issues with this were that
    - because OIDC auth is handled by the ALB, which acts as a "Backend for
    Frontend" token handler, the client should only be concerned with the
    token handler session cookie and never receive a JWT
    - /whoami is not RESTful in that it was not returning a resource
    
    `/users/current` DOES return a resource, the object of a single user
    which is identified by way of the token which is forwarded to the api by
    the token handler mapped to the session cookie

--- backend/cmd/api/internal/controller/whoami.go
+++
@@ -1,8 +0,0 @@
-package controller
-
-import "net/http"
-
-func WhoAmI(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/jwt")
-	w.Write([]byte(r.Header[http.CanonicalHeaderKey("authorization")][0]))
-}
