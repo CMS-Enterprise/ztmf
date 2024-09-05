164fb7397fefbe4353b4d3f6bf0eba7eee4e8c35
Richard Jones <richard.jones@aquia.io>
2024-09-05 15:09:12 -0600

    adds ability to list, create, and update users

--- backend/cmd/api/internal/controller/users.go
+++ backend/cmd/api/internal/controller/users.go
@@ -1,12 +1,26 @@
 package controller
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/CMS-Enterprise/ztmf/backend/cmd/api/internal/auth"
 	"github.com/CMS-Enterprise/ztmf/backend/cmd/api/internal/model"
 	"github.com/gorilla/mux"
 )
+
+func ListUsers(w http.ResponseWriter, r *http.Request) {
+	// TODO: replace the repititious admin checks with ACL
+	authdUser := auth.UserFromContext(r.Context())
+	if !authdUser.IsAdmin() {
+		respond(w, nil, &ForbiddenError{})
+		return
+	}
+
+	users, err := model.FindUsers(r.Context())
+
+	respond(w, users, err)
+}
 
 func GetUserById(w http.ResponseWriter, r *http.Request) {
 	authdUser := auth.UserFromContext(r.Context())
@@ -29,6 +43,56 @@ func GetUserById(w http.ResponseWriter, r *http.Request) {
 
 func GetCurrentUser(w http.ResponseWriter, r *http.Request) {
 	user := auth.UserFromContext(r.Context())
+
+	respond(w, user, nil)
+}
+
+// SaveUser is for admin management
+func SaveUser(w http.ResponseWriter, r *http.Request) {
+	authdUser := auth.UserFromContext(r.Context())
+	if !authdUser.IsAdmin() {
+		respond(w, nil, &ForbiddenError{})
+		return
+	}
+
+	user := &model.User{}
+
+	err := getJSON(r.Body, user)
+	if err != nil {
+		log.Println(err)
+		respond(w, nil, err)
+		return
+	}
+
+	err = validateEmail(user.Email)
+	if err != nil {
+		respond(w, nil, err)
+		return
+	}
+
+	err = validateRole(user.Role)
+	if err != nil {
+		respond(w, nil, err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	if v, ok := vars["userid"]; ok {
+		user.UserID = v
+	}
+
+	if user.UserID != "" {
+		user, err = model.UpdateUser(r.Context(), *user)
+	} else {
+		user, err = model.CreateUser(r.Context(), *user)
+	}
+
+	if err != nil {
+		log.Println(err)
+		// TODO: wrap all such db errors to return generic 500 to client but still log it
+		respond(w, nil, err)
+		return
+	}
 
 	respond(w, user, nil)
 }
