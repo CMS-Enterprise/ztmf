f5bfd4850a21a839a168776cf50b037281de466a
Richard Jones <richard.jones@aquia.io>
2024-10-11 15:09:25 -0600

    final adjustments to spreadsheet format

--- backend/cmd/api/internal/spreadsheet/spreadsheet.go
+++ backend/cmd/api/internal/spreadsheet/spreadsheet.go
@@ -17,11 +17,11 @@ func Excel(answers []*model.Answer) (*excelize.File, error) {
 	f.SetCellValue(sheet, "B1", "Data Center Environment")
 	f.SetCellValue(sheet, "C1", "Pillar")
 	f.SetCellValue(sheet, "D1", "Function")
-	f.SetCellValue(sheet, "E1", "Question")
-	f.SetCellValue(sheet, "F1", "Description")
+	f.SetCellValue(sheet, "E1", "Function Description")
+	f.SetCellValue(sheet, "F1", "Question")
 	f.SetCellValue(sheet, "G1", "Answer")
 	f.SetCellValue(sheet, "H1", "Score")
-	f.SetCellValue(sheet, "I1", "Notes")
+	f.SetCellValue(sheet, "I1", "ADO Answer Details")
 
 	for i, a := range answers {
 		row := i + 2 // i starts at 0 and headers are in row 1
@@ -29,8 +29,8 @@ func Excel(answers []*model.Answer) (*excelize.File, error) {
 		f.SetCellValue(sheet, fmt.Sprintf("B%d", row), a.DataCenterEnvironment)
 		f.SetCellValue(sheet, fmt.Sprintf("C%d", row), a.Pillar)
 		f.SetCellValue(sheet, fmt.Sprintf("D%d", row), a.Function)
-		f.SetCellValue(sheet, fmt.Sprintf("E%d", row), a.Question)
-		f.SetCellValue(sheet, fmt.Sprintf("F%d", row), a.Description)
+		f.SetCellValue(sheet, fmt.Sprintf("E%d", row), a.Description)
+		f.SetCellValue(sheet, fmt.Sprintf("F%d", row), a.Question)
 		f.SetCellValue(sheet, fmt.Sprintf("G%d", row), a.OptionName)
 		f.SetCellValue(sheet, fmt.Sprintf("H%d", row), a.Score)
 		f.SetCellValue(sheet, fmt.Sprintf("I%d", row), a.Notes)
