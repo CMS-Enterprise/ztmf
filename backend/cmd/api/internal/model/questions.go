1017c0baa0b3046c543521614a543d26929563e0
Richard A. Jones <richard.jones@aquia.io>
2024-11-15 12:36:59 -0700

    feat: admins have ability to add and edit questions (#148)
    ## Added
    - controller.SaveQuestion
    - controller.GetQuestionByID
    - model.FindQuestionByID
    - (model.Question).Save()
    
    ## Changed
    - refactored previous handling of FindQuestions to be clearly
    FindQuestionsByFismaSystem since they serve 2 different purposes.
    FindQuestions is for admin management, and the latter is for users
    listing questions with joined data such as function and pillar
    
    
    
    closes #129

--- backend/cmd/api/internal/model/questions.go
+++ backend/cmd/api/internal/model/questions.go
@@ -3,42 +3,71 @@ package model
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/jackc/pgx/v5"
 )
 
+var questionsColumns = []string{"questionid", "question", "notesprompt", "ordr", "pillarid"}
+
 // Question represents a record from the questions table
 type Question struct {
-	QuestionID  int32    `json:"questionid"`
-	Question    string   `json:"question"`
-	Notesprompt string   `json:"notesprompt"`
-	Order       int      `json:"order"`
-	Pillar      Pillar   `json:"pillar"`
-	Function    Function `json:"function"`
+	QuestionID  int32     `json:"questionid"`
+	Question    string    `json:"question"`
+	NotesPrompt string    `json:"notesprompt"`
+	Order       int       `json:"order"`
+	PillarID    int       `json:"pillarid"`
+	Pillar      *Pillar   `json:"pillar,omitempty"`
+	Function    *Function `json:"function,omitempty"`
 }
 
-// FindQuestionsInput provides fields to filter questions by. These are applied in the WHERE clause.
-type FindQuestionsInput struct {
-	FismaSystemID *int32
-}
+func (q *Question) Save(ctx context.Context) error {
 
-// FindQuestions performs a select from questions table and returns records as an array of *Questions
-func FindQuestions(ctx context.Context, input FindQuestionsInput) ([]*Question, error) {
-	sqlb := sqlBuilder.
-		Select("questions.questionid, question, notesprompt, questions.ordr, pillars.pillarid, pillars.pillar, pillars.ordr").
-		From("questions").
-		InnerJoin("pillars ON pillars.pillarid=questions.pillarid")
+	var (
+		sql       string
+		boundArgs []any
+		err       error
+	)
 
-	if input.FismaSystemID != nil {
-		sqlb = sqlb.
-			Columns("functionid, function, description").
-			InnerJoin("functions ON functions.questionid=questions.questionid").
-			InnerJoin("fismasystems ON fismasystems.datacenterenvironment=functions.datacenterenvironment AND fismasystems.fismasystemid=?", *input.FismaSystemID)
+	if q.QuestionID == 0 {
+		sql, boundArgs, _ = sqlBuilder.
+			Insert("questions").
+			Columns(questionsColumns[1:]...).
+			Values(q.Question, q.NotesPrompt, q.Order, q.PillarID).
+			Suffix("RETURNING " + strings.Join(questionsColumns, ", ")).
+			ToSql()
+	} else {
+		sql, boundArgs, _ = sqlBuilder.Update("questions").
+			Set("question", q.Question).
+			Set("notesprompt", q.NotesPrompt).
+			Set("ordr", q.Order).
+			Set("pillarid", q.PillarID).
+			Where("questionid=?", q.QuestionID).
+			Suffix("RETURNING " + strings.Join(questionsColumns, ", ")).
+			ToSql()
 	}
 
-	sqlb = sqlb.OrderBy("pillars.ordr, questions.ordr ASC")
+	row, err := queryRow(ctx, sql, boundArgs...)
+	if err != nil {
+		return trapError(err)
+	}
 
-	sql, boundArgs, _ := sqlb.ToSql()
+	err = row.Scan(&q.QuestionID, &q.Question, &q.NotesPrompt, &q.Order, &q.PillarID)
+
+	return trapError(err)
+}
+
+// func (q *Question) isValid() (bool, error) {
+// 	return true, nil
+// }
+
+// FindQuestions returns questions without joins, it is used by admins for management
+func FindQuestions(ctx context.Context) ([]*Question, error) {
+	sql, boundArgs, _ := sqlBuilder.
+		Select(questionsColumns...).
+		From("questions").
+		ToSql()
+
 	rows, err := query(ctx, sql, boundArgs...)
 
 	if err != nil {
@@ -47,23 +76,56 @@ func FindQuestions(ctx context.Context, input FindQuestionsInput) ([]*Question, 
 	}
 
 	return pgx.CollectRows(rows, func(row pgx.CollectableRow) (*Question, error) {
-		question := Question{}
-		scanFields := []any{
-			&question.QuestionID,
-			&question.Question,
-			&question.Notesprompt,
-			&question.Order,
-			&question.Pillar.PillarID,
-			&question.Pillar.Pillar,
-			&question.Pillar.Order,
-		}
+		q := Question{}
+		err := row.Scan(&q.QuestionID, &q.Question, &q.NotesPrompt, &q.Order, &q.PillarID)
+		return &q, trapError(err)
+	})
+}
 
-		if input.FismaSystemID != nil {
-			question.Function = Function{}
-			scanFields = append(scanFields, &question.Function.FunctionID, &question.Function.Function, &question.Function.Description)
-		}
+func FindQuestionByID(ctx context.Context, questionID int32) (*Question, error) {
+	sql, boundArgs, _ := sqlBuilder.
+		Select(questionsColumns...).
+		From("questions").
+		Where("questionid=?", questionID).
+		ToSql()
 
-		err := rows.Scan(scanFields...)
-		return &question, trapError(err)
+	row, err := queryRow(ctx, sql, boundArgs...)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	q := Question{}
+	err = row.Scan(&q.QuestionID, &q.Question, &q.NotesPrompt, &q.Order, &q.PillarID)
+	return &q, err
+}
+
+// FindQuestionsByFismaSystem joins questions with functions to return questions relevant to the fismasystem as determined by the datacenterenvironment.
+// It is used by all users to list questions relevant to the specified fisma system
+func FindQuestionsByFismaSystem(ctx context.Context, fismaSystemID int32) ([]*Question, error) {
+	sql, boundArgs, _ := sqlBuilder.
+		Select("questions.questionid, question, notesprompt, questions.ordr, pillars.pillarid, pillars.pillar, pillars.ordr, functionid, function, description").
+		From("questions").
+		InnerJoin("pillars ON pillars.pillarid=questions.pillarid").
+		InnerJoin("functions ON functions.questionid=questions.questionid").
+		InnerJoin("fismasystems ON fismasystems.datacenterenvironment=functions.datacenterenvironment AND fismasystems.fismasystemid=?", fismaSystemID).
+		OrderBy("pillars.ordr, questions.ordr ASC").
+		ToSql()
+
+	rows, err := query(ctx, sql, boundArgs...)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return pgx.CollectRows(rows, func(row pgx.CollectableRow) (*Question, error) {
+		q := Question{
+			Pillar:   &Pillar{},
+			Function: &Function{},
+		}
+		err := row.Scan(&q.QuestionID, &q.Question, &q.NotesPrompt, &q.Order, &q.Pillar.PillarID, &q.Pillar.Pillar, &q.Pillar.Order, &q.Function.FunctionID, &q.Function.Function, &q.Function.Description)
+		return &q, trapError(err)
 	})
 }
