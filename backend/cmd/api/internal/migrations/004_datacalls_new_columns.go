15587f0542d44c38a08321f415b98f37edd3bc76
Richard Jones <richard.jones@aquia.io>
2024-11-22 13:40:13 -0700

    added new fields for email sending operations

---
+++ backend/cmd/api/internal/migrations/004_datacalls_new_columns.go
@@ -0,0 +1,14 @@
+package migrations
+
+func init() {
+	getMigrator().AppendMigration(
+		"datacalls new columnds",
+		`ALTER TABLE IF EXISTS public.datacalls ADD COLUMN IF NOT EXISTS "emailsubject" varchar(100);
+		ALTER TABLE IF EXISTS public.datacalls ADD COLUMN IF NOT EXISTS "emailbody" varchar(2000);
+		ALTER TABLE IF EXISTS public.datacalls ADD COLUMN IF NOT EXISTS "emailsent" timestamp with time zone[];
+		`,
+		`ALTER TABLE IF EXISTS public.functions DROP COLUMN IF EXISTS emailsubject;
+		ALTER TABLE IF EXISTS public.functions DROP COLUMN IF EXISTS emailbody;
+		ALTER TABLE IF EXISTS public.functions DROP COLUMN IF EXISTS emailsent;
+		`)
+}
--- backend/cmd/api/internal/model/datacalls.go
+++ backend/cmd/api/internal/model/datacalls.go
@@ -9,17 +9,20 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-var dataCallColumns = []string{"datacallid", "datacall", "datecreated", "deadline"}
+var dataCallColumns = []string{"datacallid", "datacall", "datecreated", "deadline", "emailsubject", "emailbody", "emailsent"}
 
 type DataCall struct {
-	DataCallID  int32     `json:"datacallid"`
-	DataCall    string    `json:"datacall"`
-	DateCreated time.Time `json:"datecreated"`
-	Deadline    time.Time `json:"deadline"`
+	DataCallID   int32     `json:"datacallid"`
+	DataCall     string    `json:"datacall"`
+	DateCreated  time.Time `json:"datecreated"`
+	Deadline     time.Time `json:"deadline"`
+	EmailSubject *string   `json:"emailsubject"`
+	EmailBody    *string   `json:"emailbody"`
+	EmailSent    *string   `json:"emailsent"`
 }
 
 func (d *DataCall) fields() []any {
-	return []any{&d.DataCallID, &d.DataCall, &d.DateCreated, &d.Deadline}
+	return []any{&d.DataCallID, &d.DataCall, &d.DateCreated, &d.Deadline, &d.EmailSubject, &d.EmailBody, &d.EmailSent}
 }
 
 func (d *DataCall) Save(ctx context.Context) error {
@@ -37,8 +40,8 @@ func (d *DataCall) Save(ctx context.Context) error {
 	if d.DataCallID == 0 {
 		sql, boundArgs, _ = sqlBuilder.
 			Insert("datacalls").
-			Columns("datacall", "deadline").
-			Values(d.DataCall, d.Deadline).
+			Columns("datacall", "deadline", "emailsubject", "emailbody").
+			Values(d.DataCall, d.Deadline, d.EmailSubject, d.EmailBody).
 			Suffix("RETURNING " + strings.Join(dataCallColumns, ", ")).
 			ToSql()
 	} else {
@@ -46,6 +49,8 @@ func (d *DataCall) Save(ctx context.Context) error {
 			Update("datacalls").
 			Set("datacall", d.DataCall).
 			Set("deadline", d.Deadline).
+			Set("emailsubject", d.EmailSubject).
+			Set("emailbody", d.EmailBody).
 			Where("datacallid=?", d.DataCallID).
 			Suffix("RETURNING " + strings.Join(dataCallColumns, ", ")).
 			ToSql()
