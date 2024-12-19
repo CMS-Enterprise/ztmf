f8bb81e18fb9ee889336812cac5e6acb4c9ade46
Richard Jones <richard.jones@aquia.io>
2024-12-19 11:46:23 -0700

    refactored validate and writes

--- backend/cmd/api/internal/model/usersfismasystems.go
+++ backend/cmd/api/internal/model/usersfismasystems.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"context"
-	"log"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -12,61 +11,7 @@ type UserFismaSystem struct {
 	FismaSystemID int32  `json:"fismasystemid"`
 }
 
-// FindUserFismaSystemsByUserID queries the user_fismasystems table to return a list of fismasystemids associated with the userID
-func FindUserFismaSystemsByUserID(ctx context.Context, userid string) (*[]int32, error) {
-	if !isValidUUID(userid) {
-		return nil, ErrNoData
-	}
-
-	sqlb := stmntBuilder.Select("ARRAY_AGG(fismasystemid) as fismasystemids").
-		From("users_fismasystems").
-		Where("userid=?", userid)
-
-	return queryRow(ctx, sqlb, pgx.RowTo[[]int32])
-}
-
-// AddUserFismaSystem inserts a record into the users_fismasystems table
-func AddUserFismaSystem(ctx context.Context, uf UserFismaSystem) error {
-
-	err := validateUserFismasystem(uf)
-	if err != nil {
-		return err
-	}
-
-	sqlb := stmntBuilder.Insert("userid, fismasystemid").
-		Into("users_fismasystems").
-		Values(uf.UserID, uf.FismaSystemID).
-		Suffix("ON CONFLICT DO NOTHING")
-
-	err = exec(ctx, sqlb)
-	if err != nil {
-		return trapError(err)
-	}
-
-	return nil
-}
-
-func DeleteUserFismaSystem(ctx context.Context, uf UserFismaSystem) error {
-
-	err := validateUserFismasystem(uf)
-	if err != nil {
-		return err
-	}
-
-	sqlb := stmntBuilder.
-		Delete("users_fismasystems").
-		Where("userid=? AND fismasystemid=?", uf.UserID, uf.FismaSystemID)
-
-	err = exec(ctx, sqlb)
-	if err != nil {
-		log.Println(err)
-		return trapError(err)
-	}
-
-	return nil
-}
-
-func validateUserFismasystem(uf UserFismaSystem) error {
+func (uf *UserFismaSystem) validate() error {
 	inputErr := InvalidInputError{data: map[string]any{}}
 
 	if !isValidUUID(uf.UserID) {
@@ -82,4 +27,50 @@ func validateUserFismasystem(uf UserFismaSystem) error {
 	}
 
 	return nil
+}
+
+// AddUserFismaSystem inserts a record into the users_fismasystems table
+func (uf *UserFismaSystem) Save(ctx context.Context) (*UserFismaSystem, error) {
+
+	err := uf.validate()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlb := stmntBuilder.Insert("userid, fismasystemid").
+		Into("users_fismasystems").
+		Values(uf.UserID, uf.FismaSystemID).
+		Suffix("ON CONFLICT DO NOTHING RETURNING userid, fismasystemid")
+
+	return queryRow(ctx, sqlb, pgx.RowToStructByName[UserFismaSystem])
+}
+
+func (uf *UserFismaSystem) Delete(ctx context.Context) error {
+
+	err := uf.validate()
+	if err != nil {
+		return err
+	}
+
+	sqlb := stmntBuilder.
+		Delete("users_fismasystems").
+		Where("userid=? AND fismasystemid=?", uf.UserID, uf.FismaSystemID).
+		Suffix("RETURNING userid, fismasystemid")
+
+	_, err = queryRow(ctx, sqlb, pgx.RowToStructByName[UserFismaSystem])
+
+	return err
+}
+
+// FindUserFismaSystemsByUserID queries the user_fismasystems table to return a list of fismasystemids associated with the userID
+func FindUserFismaSystemsByUserID(ctx context.Context, userid string) (*[]int32, error) {
+	if !isValidUUID(userid) {
+		return nil, ErrNoData
+	}
+
+	sqlb := stmntBuilder.Select("ARRAY_AGG(fismasystemid) as fismasystemids").
+		From("users_fismasystems").
+		Where("userid=?", userid)
+
+	return queryRow(ctx, sqlb, pgx.RowTo[[]int32])
 }
