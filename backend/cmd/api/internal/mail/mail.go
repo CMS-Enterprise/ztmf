039de1b291314d3f08425524b8fa83a2bff97c49
Richard Jones <richard.jones@aquia.io>
2025-03-04 12:02:45 -0700

    added log messages

--- backend/cmd/api/internal/mail/mail.go
+++ backend/cmd/api/internal/mail/mail.go
@@ -17,26 +17,28 @@ import (
 // it is meant to run as a background go routine and therefore logs errors rather than returning them
 func Send(subject, body string) {
 	var (
-		contacts []*model.DataCallContact
-		tlsCfg   *tls.Config
+		contacts   []*model.DataCallContact
+		tlsCfg     *tls.Config
+		emailsSent int
 	)
-
 	cfg := config.GetInstance()
 
 	if cfg.SMTP.Certs != nil {
 		tlsCfg = &tls.Config{RootCAs: cfg.SMTP.Certs}
 	}
 
+	log.Println("dialing SMTP server...")
 	c, err := smtp.DialStartTLS(fmt.Sprintf("%s:%d", cfg.SMTP.Host, cfg.SMTP.Port), tlsCfg)
-	defer c.Quit()
 
 	if err != nil {
 		log.Println("error dialing tls: ", err)
 		return
 	}
+	defer c.Quit()
 
 	auth := sasl.NewPlainClient("ztmfapi", cfg.SMTP.User, cfg.SMTP.Pass)
 
+	log.Println("authenticating to SMTP server...")
 	err = c.Auth(auth)
 
 	if err != nil {
@@ -44,6 +46,7 @@ func Send(subject, body string) {
 		return
 	}
 
+	log.Println("finding email contacts...")
 	if cfg.SMTP.TestMode {
 		contacts, err = model.FindTestDataCallContacts(context.Background())
 	} else {
@@ -57,11 +60,16 @@ func Send(subject, body string) {
 
 	msg := strings.NewReader("")
 
+	log.Println("sending emails...")
+
 	for _, contact := range contacts {
 		msg.Reset("To: " + contact.Email + "\r\n" + "Subject: " + subject + "\r\n" + "\r\n" + body + "\r\n")
 		err = c.SendMail(cfg.SMTP.From, []string{contact.Email}, msg)
 		if err != nil {
 			log.Println("error sending email: ", err)
+		} else {
+			emailsSent++
 		}
 	}
+	log.Printf("sent %d emails", emailsSent)
 }
