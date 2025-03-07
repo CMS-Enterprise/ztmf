c3e81fd0e24502cbf72588a3fe9feb615d115cc3
Richard Jones <richard.jones@aquia.io>
2025-03-06 17:05:34 -0700

    initial implementation of exhaustive api tests based on emberfall v0.3.0

--- backend/_test_data.sql
+++ backend/_test_data.sql
@@ -1,5 +1,3 @@
 
 INSERT INTO public.users VALUES (DEFAULT, 'test.user@nowhere.xyz', 'Admin User', 'ADMIN') ON CONFLICT DO NOTHING;
-INSERT INTO public.fismasystems VALUES (DEFAULT, 'abcdefghijklmnopqrstuvwxyz', 'TEST', 'Test Environment System Test', 'test', 'test', 'test group acronym', 'test group', 'test division', 'OTHER', 'test contact', 'test@nowhere.xyz') ON CONFLICT DO NOTHING;
 INSERT INTO public.pillars VALUES (DEFAULT, 'TEST pillar', 1);
-INSERT INTO public.functions VALUES (DEFAULT, 'TEST FUNCTION', 'Test function description', 'AWS', null, 1, 1);
